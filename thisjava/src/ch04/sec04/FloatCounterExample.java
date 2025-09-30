f0d1ac6fbdd9c45cec8140df68bedb8a687e090a
lkd5565 <dwerap21@naver.com>
2025-09-30 21:34:01 +0900

    4주차 실습

---
+++ thisjava/src/ch04/sec04/FloatCounterExample.java
@@ -0,0 +1,9 @@
+package ch04.sec04;
+
+public class FloatCounterExample {
+    public static void main(String[] args) {
+        for(float x=01.f;x<=1.0f;x+=0.1f){
+            System.out.println(x);
+        }
+    }
+}
